389c735c45637a7cfa36c684e5b62e318849dbe5
srimanth bolla <srimanthbolla9126@gmail.com>
2026-01-27 18:37:06 -0500

    ansible-roles

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -31,7 +31,47 @@ resource "terraform_data" "mongodb" {
   provisioner "remote-exec" {
     inline = [ 
       "chmod +x /tmp/bootstrap.sh",
-      "sudo sh /tmp/bootstrap.sh"
+      #"sudo sh /tmp/bootstrap.sh"
+      "sudo sh /tmp/bootstrap.sh mongodb"
+     ]
+     }
+}
+
+#redis
+resource "aws_instance" "redis" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.redis_sg_id]
+    subnet_id = local.database_subnet_ids
+    tags = merge (
+      local.common_tags,
+      {
+        Name = "${local.common_name_suffix}-redis"  # roboshop-dev-redis
+      } 
+    )
+
+}
+
+resource "terraform_data" "redis" {
+  triggers_replace = [
+    aws_instance.redis.id
+  ]
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    password = "DevOps321"
+    host        = aws_instance.redis.private_ip
+    
+  }
+  #Terraform copies this file to redis server take connection from above
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+  provisioner "remote-exec" {
+    inline = [ 
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh redis"
      ]
      }
 }
