58058398c3619567875311aa6058ca417ac046e2
srimanth bolla <srimanthbolla9126@gmail.com>
2026-01-27 18:40:48 -0500

    ansible-roles

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -26,3 +26,12 @@ resource "aws_security_group_rule" "mongodb_bastion" {
   protocol          = "tcp"
   to_port           = 22
 }
+#redis SG allowing traffic from bastion
+resource "aws_security_group_rule" "redis_bastion" {
+  type              = "ingress"
+  security_group_id = local.redis_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
