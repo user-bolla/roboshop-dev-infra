b0902c364c43906c31145e7f2fc6ac8ba02a046b
srimanth bolla <srimanthbolla9126@gmail.com>
2026-02-02 13:30:32 -0500

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -3,7 +3,7 @@ component=$1
 dnf install ansible -y
 #ansible-pull -U https://github.com/user-bolla/ansible-roboshop-roles-tf-main.git -e component=$component main.yaml
 
-REPO_URL=https://github.com/user-bolla/ansible-roboshop-roles-tf.git
+REPO_URL=https://github.com/user-bolla/ansible-roboshop-roles-tf-main.git
 REPO_DIR=/opt/roboshop-ansible
 ANSIBLE_DIR=ansible-roboshop-roles-tf
 
