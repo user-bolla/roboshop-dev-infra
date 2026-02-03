84af7261606af5cb9d686a3855b037426a13f727
srimanth bolla <srimanthbolla9126@gmail.com>
2026-02-02 19:29:53 -0500

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -22,4 +22,4 @@ else
   cd $ANSIBLE_DIR
 fi
 
-ansible-playbook -e component=$component -e environment=$environment main.yaml
+ansible-playbook -e component=$component -e env=$environment main.yaml
