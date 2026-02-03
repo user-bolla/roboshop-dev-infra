38a7b7de76e0599fae05a8aacdf63c5963236b49
srimanth bolla <srimanthbolla9126@gmail.com>
2026-02-03 11:18:03 -0500

    terraform

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "bastion" {
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.bastion_sg_id]
     subnet_id = local.public_subnet_id
+    iam_instance_profile = aws_iam_instance_profile.bastion.name
     user_data = file("bastion.sh")
     tags = merge (
       local.common_tags,
@@ -10,4 +11,12 @@ resource "aws_instance" "bastion" {
         Name = "${var.project_name}-${var.environment}-bastion"
       } 
     )
+}
+
+
+
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "bastion" 
+  role = "BastionTerraformAdmin"
 }
