ee1fd46f0db5675744fe281ae4291eac88992fa0
srimanth bolla <srimanthbolla9126@gmail.com>
2026-02-03 12:28:40 -0500

    terraform

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -53,3 +53,13 @@ resource "aws_security_group_rule" "mysql_bastion" {
   protocol          = "tcp"
   to_port           = 22
 }
+
+#catalogue SG allowing traffic from bastion
+resource "aws_security_group_rule" "catalogue_bastion" {
+  type              = "ingress"
+  security_group_id = local.catalogue_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
