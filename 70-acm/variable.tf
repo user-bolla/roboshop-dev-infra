d8b57bc216826dcae9e3151a77f55dee1193482e
srimanth bolla <srimanthbolla9126@gmail.com>
2026-02-10 12:02:32 -0500

    terraform

---
+++ 70-acm/variable.tf
@@ -0,0 +1,13 @@
+variable "project_name" {
+  default = "roboshop"
+}
+variable "environment" {
+  default = "dev"
+}
+variable "domain_name" {
+  default = "userbolla.store"
+}
+
+variable "zone_id" {
+  default = "Z09862333DG9EIMMVCYEW"
+}
