9cd0df863b78388d5fd814288bda067cfcbfb522
srimanth bolla <srimanthbolla9126@gmail.com>
2026-02-11 10:06:52 -0500

    terraform

---
+++ 90-components/main.tf
@@ -0,0 +1,5 @@
+module "components" {
+  source = "../terraform-roboshop-components"
+  component = var.component
+  rule_priority = var.rule_priority
+}
---
+++ 90-components/variables.tf
@@ -0,0 +1,6 @@
+variable "component" {
+  default = "catalogue"
+}
+variable "rule_priority" {
+  default = 10
+}
