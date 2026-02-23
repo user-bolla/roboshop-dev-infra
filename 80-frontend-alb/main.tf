f1de6964f9f63f4a8c4404c08c3079c021123e7d
srimanth bolla <srimanthbolla9126@gmail.com>
2026-02-23 13:19:33 -0500

    terraform

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -38,7 +38,7 @@ resource "aws_lb_listener" "frontend_alb" {
 
 resource "aws_route53_record" "frontend_alb" {
   zone_id = var.zone_id
-  name    = "*.roboshop-${var.environment}.${var.domain_name}" #roboshop-dev.userbolla.store
+  name    = "roboshop-${var.environment}.${var.domain_name}" #roboshop-dev.userbolla.store
   type    = "A"
   allow_overwrite = true
   alias {
